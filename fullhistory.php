c50dbd3d67f490a0f9b0c3129b01f52c352187da
Jamey Sharp <jamey@minilop.net>
2018-06-29 16:51:01 -0700

    Bump archive-feed URLs when they change.
    I think this version should satisfy all the requirements of RFC5005
    section 4.

--- fullhistory.php
+++ fullhistory.php
@@ -2,12 +2,122 @@
 /*
 Plugin Name:  Full-history (RFC5005) feeds
 Description:  Allow RFC5005-capable feed readers to see all posts, not just the most recent.
-Version:      0.1
+Version:      0.2
 Author:       Jamey Sharp
 Author URI:   https://jamey.thesharps.us/
 License:      BSD-2-Clause
 License URI:  https://www.freebsd.org/copyright/freebsd-license.html
 */
+
+/**
+ * Test if two arrays have no values in common.
+ *
+ * @param array $a First array.
+ * @param array $b Second array.
+ * @return bool False if any element in $a is also in $b, true otherwise.
+ */
+function fullhistory_disjoint( $a, $b ) {
+	foreach ( $a as $element ) {
+		if ( in_array( $element, $b, true ) ) {
+			return false;
+		}
+	}
+	return true;
+}
+
+/**
+ * Invalidate feed reader caches by changing the URL we use to link to any
+ * archived feed pages that might have been affected.
+ *
+ * @param array $query_params Query parameters to pass to WP_Query to limit
+ *                            which posts might be part of a page that should
+ *                            be re-fetched.
+ */
+function fullhistory_invalidate_client_caches( $query_params = array() ) {
+	$public_stati = get_post_stati( array( 'public' => true ) );
+
+	$change_count = get_option( 'fullhistory_change_count', 1 );
+	++$change_count;
+	update_option( 'fullhistory_change_count', $change_count );
+
+	$query_params = array_merge(
+		$query_params,
+		array(
+			'fields'        => 'ids',
+			'no_found_rows' => true,
+			'nopaging'      => true,
+			'post_status'   => $public_stati,
+		)
+	);
+
+	$query = new WP_Query( $query_params );
+	foreach ( $query->posts as $id ) {
+		update_post_meta( $id, '_fullhistory_version', $change_count );
+	}
+}
+
+add_action( 'update_option_posts_per_rss', 'fullhistory_invalidate_client_caches', 10, 0 );
+
+/**
+ * When a post changes, invalidate feed reader client caches as needed.
+ *
+ * Posts that are not visible in a feed either before or after this change
+ * don't need to invalidate any caches, so this function takes the list of the
+ * old and new status of the changed post. If the post is being deleted, give
+ * only the old status.
+ *
+ * @param WP_Post $post       The changed post.
+ * @param array   $post_stati The post's status, both old and new.
+ * @param array   $exclude    Post IDs to not invalidate.
+ */
+function fullhistory_post_change( $post, $post_stati, $exclude = array() ) {
+	$public_stati = get_post_stati( array( 'public' => true ) );
+	if ( fullhistory_disjoint( $post_stati, $public_stati ) ) {
+		return;
+	}
+
+	fullhistory_invalidate_client_caches(
+		array(
+			'post__not_in' => $exclude,
+			'date_query'   => array(
+				'after'     => $post->post_date,
+				'inclusive' => true,
+			),
+		)
+	);
+}
+
+/**
+ * Invalidate client caches on 'transition_post_status' action.
+ *
+ * Note that this action fires every time a post is saved, even if its status
+ * hasn't actually changed.
+ *
+ * @param string  $new_status The post's new status.
+ * @param string  $old_status The post's old status (possibly the same).
+ * @param WP_Post $post       The post that is being saved.
+ */
+function fullhistory_post_status_change( $new_status, $old_status, $post ) {
+	fullhistory_post_change( $post, array( $new_status, $old_status ) );
+}
+
+add_action( 'transition_post_status', 'fullhistory_post_status_change', 10, 3 );
+
+/**
+ * Invalidate client caches on 'delete_post' action.
+ *
+ * We have to exclude this post from the invalidation because we're in the
+ * middle of deleting it so we need to avoid touching anything related to it in
+ * the database.
+ *
+ * @param int $postid The ID of the post that's being deleted.
+ */
+function fullhistory_post_delete( $postid ) {
+	$post = get_post( $postid );
+	fullhistory_post_change( $post, array( $post->post_status ), array( $post->ID ) );
+}
+
+add_action( 'delete_post', 'fullhistory_post_delete', 10, 1 );
 
 /**
  * Add the standard RFC5005 namespace to a feed root element.
@@ -103,12 +213,6 @@ function fullhistory_xml_head() {
 	// Only page 2 and later can have a prev-archive link, since there is
 	// no archive earlier than page 1.
 	if ( $current_page > 1 ) {
-		// FIXME: Add a "cache-busting" query string parameter such
-		// that, when a post is saved or deleted, the pages for that
-		// post and everything newer all get new URLs. Until that's
-		// implemented, RFC5005-compliant feed readers may not notice
-		// when old posts are edited, deleted, or inserted.
-		//
 		// According to the standard, archived feeds are effectively
 		// treated as if they have a far-future Expires: header, which
 		// means in order to invalidate caches we need to use the same
@@ -116,7 +220,28 @@ function fullhistory_xml_head() {
 		// files. See
 		// https://css-tricks.com/strategies-for-cache-busting-css/ for
 		// example.
-		$prev_archive = add_query_arg( 'order', 'ASC', $current_feed );
+		$prev_query   = new WP_Query(
+			array(
+				'fields'        => 'ids',
+				'no_found_rows' => true,
+				'feed'          => 'feed',
+				'order'         => 'ASC',
+				'posts_per_rss' => 1,
+				'offset'        => ( $current_page - 1 ) * $per_page - 1,
+			)
+		);
+		$change_count = get_post_meta( $prev_query->posts[0], '_fullhistory_version', true );
+		if ( empty( $change_count ) ) {
+			$change_count = 1;
+		}
+
+		$prev_archive = add_query_arg(
+			array(
+				'order'       => 'ASC',
+				'fullhistory' => $change_count,
+			),
+			$current_feed
+		);
 
 		// Linking to page 1 is special because if you pass 'paged=1',
 		// WordPress redirects to a canonical URL with that page number
